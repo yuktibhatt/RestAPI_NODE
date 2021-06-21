1b80fcd6efeb31c9b4541793d6e982df86876d01
yuktibhatt <bhattyukti5@gmail.com>
2021-06-22 00:04:26 +0545

    get method

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const { Client } = require('pg');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyparser.json());
+app.use(cors());
 
 const client = new Client({
     user: "postgres",
@@ -8,10 +15,34 @@ const client = new Client({
     database: "empdb"
 })
 
-client.connect()
-.then(() => console.log("Connected Successfully"))
-.then(() => client.query("select * from employee"))
-.then(results => console.table(results.rows))
-.catch( e => console.log)
-.finally(() => client.end())
+client.connect((err) => {
+    if(!err)
+    console.log("Db connection successful");
+    else
+    console.log("Db connections fails \n Error: " + Json.stringify(err, undefined, 2));
+});
+
+app.listen(8000, () => console.log('Exress server up'));
+
+//get method 
+app.get('/users', (req,res) => {
+    client.query('SELECT * FROM users', (err, rows, fields) => {
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);   
+    })
+});
+
+//get an employee
+app.get('/users/:id', (req,res) => {
+    const id = req.params.id;
+    client.query(`SELECT * FROM users WHERE userID = ${id}`, (err, response) => {
+        if(!err)
+        res.send(response.rows);
+        else
+        console.log(err);   
+    })
+});
+
 
